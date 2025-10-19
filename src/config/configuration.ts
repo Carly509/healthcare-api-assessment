3778a34a0e0b4c67afe18452479aeb7d8378cdb2
carly509 <tesnorcarly@gmail.com>
2025-10-19 19:12:46 -0400

    fix

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,9 @@
 export default () => ({
   api: {
-    baseUrl: process.env.API_BASE_URL,
-    apiKey: process.env.API_KEY,
+    baseUrl:
+      process.env.API_BASE_URL || 'https://assessment.ksensetech.com/api',
+    apiKey:
+      process.env.API_KEY ||
+      'ak_27661c1f610628248b2c8768d58644f4e5f6b36109d2c6ed',
   },
 });
